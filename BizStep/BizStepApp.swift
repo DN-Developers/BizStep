8e17a727071536ceed5e4d4111fc3d879b0e4715
DN-Developers <dndevelopers007@gmail.com>
2025-09-07 17:48:26 +0530

    Initial Commit

---
+++ BizStep/BizStepApp.swift
@@ -0,0 +1,17 @@
+//
+//  BizStepApp.swift
+//  BizStep
+//
+//  Created by Neel Kalariya on 07/09/25.
+//
+
+import SwiftUI
+
+@main
+struct BizStepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
